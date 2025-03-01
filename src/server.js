dd59f3a62f9d214bd4e3355ae2235b4d765f6839
Donjella <brendonang@gmail.com>
2025-03-01 13:04:14 +1100

    feat: cors implemented, more logging

--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
 // import express to begin using it
 const express = require("express");
 const { default: mongoose } = require("mongoose");
+const cors = require("cors");
 
 // make an instance of an express server 
 const app = express();
 
 // This allows us to send in JSON body data on our requests!
 app.use(express.json());
+
+// Configure CORS to allow requests from specific domains 
+let corsOptions = {
+	origin: ["http://localhost:5173", "https://someDeployedFrontEnd.com"],
+	credentials: true,
+	preflightContinue: true, 
+	methods: ["GET","POST","PATCH","DELETE","OPTIONS"]
+	// origin: "*"
+}
+app.use(cors(corsOptions));
+
+app.use((request, response, next) => {
+	console.log(`REQUEST: ${request.method}, PATH: ${request.path}, BODY:\n${JSON.stringify(request.body)}`);
+	next();
+});
 
 
 // configure the server instance with its routes and other middleware and so on
